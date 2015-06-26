9f55dcf4681a246a3f80310cf891413e4bda4703
Colin Mitchell <muffinista@gmail.com>
2015-06-26 10:41:14 -0400

    add rightside parser

---
+++ lib/whois/record/parser/whois.rightside.co.rb
@@ -0,0 +1,22 @@
+#--
+# Ruby Whois
+#
+# An intelligent pure Ruby WHOIS client and parser.
+#
+# Copyright (c) 2009-2014 Simone Carletti <weppos@weppos.net>
+#++
+
+
+require 'whois/record/parser/base_icann_compliant'
+
+
+module Whois
+  class Record
+    class Parser
+
+      class WhoisRightsideCo < WhoisUnitedtldCom
+      end
+
+    end
+  end
+end
